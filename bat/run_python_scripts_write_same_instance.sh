1cbb402f64fcab27dde2127621da36ac00d039f5
davidedironza <46636416+davidedironza@users.noreply.github.com>
2019-05-10 13:46:08 +0200

    Update and rename run_python_scripts_write.sh to run_python_scripts_write_same_instance.sh
    .

---
+++ bat/run_python_scripts_write_same_instance.sh
@@ -0,0 +1,12 @@
+# run directly on an instance
+
+git clone https://github.com/davidedironza/gcp_test.git
+echo "git repo cloned"
+
+cd gcp_test/source
+
+python3 python_script_write1.py
+echo "Write 1 executed"
+
+python3 python_script_write2.py
+echo "Write 2 executed"
