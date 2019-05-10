d245dbf4cc0725cffe926ad57eed7424aad01135
davidedironza <46636416+davidedironza@users.noreply.github.com>
2019-05-10 14:22:22 +0200

    Update run_python_scripts_write_same_instance.sh
    .

--- bat/run_python_scripts_write_same_instance.sh
+++ bat/run_python_scripts_write_same_instance.sh
@@ -1,9 +1,13 @@
 # run directly on an instance
 
-git clone https://github.com/davidedironza/gcp_test.git
-echo "git repo cloned"
+# Clone Repo: only necessary for the first time, then with pull
+#git clone https://github.com/davidedironza/gcp_test.git
+#echo "git repo cloned"
 
-cd gcp_test/source
+cd gcp_test
+git pull
+
+cd source
 
 python3 python_script_write1.py
 echo "Write 1 executed"
