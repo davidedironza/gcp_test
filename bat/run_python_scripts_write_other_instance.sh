262e655495783a51e19690d666d7027ed47efafc
davidedironza <46636416+davidedironza@users.noreply.github.com>
2019-05-13 17:56:54 +0200

    Update run_python_scripts_write_other_instance.sh
    .

--- bat/run_python_scripts_write_other_instance.sh
+++ bat/run_python_scripts_write_other_instance.sh
@@ -4,15 +4,4 @@
 #git clone https://github.com/davidedironza/gcp_test.git
 #echo "git repo cloned"
 
-gcloud compute ssh instance-5 --zone europe-west6-a
-
-cd gcp_test
-#git pull
-
-cd source
-
-python3 python_script_write1.py
-echo "Write 1 executed"
-
-python3 python_script_write2.py
-echo "Write 2 executed"
+gcloud compute ssh instance-5 --zone europe-west6-a -- 'cd gcp_test/source && cd python3 python_script_write1.py'
